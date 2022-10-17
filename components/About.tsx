de00e8188cd9769feccf1952cfcd36851263fd0c
gonzalo quiroga <gtquiroga@uc.cl>
2022-10-17 18:31:51 -0300

    .

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ export default function About({ pageInfo }: Props) {
         whileInView={{opacity: 1}}
         transition={{duration: 1.5}}
         className='h-screen relative flex flex-col text-center md:text-left md:flex-row
-        mx-w-7xl px-10 justify-evenly mx-auto items-center min-h-fit pt-36'
+        mx-w-7xl px-28 justify-evenly mx-auto items-center min-h-fit pt-36'
     >
         <h3 className='absolute uppercase top-24 tracking-[20px] text-gray-500 text-2xl'>About</h3>
         <motion.img
@@ -34,7 +34,7 @@ export default function About({ pageInfo }: Props) {
             }}
             src={urlFor(pageInfo?.profilePic).url()}
             className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
-            md:rounded-md md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+            md:rounded-md md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]'
         />
         <div className='space-y-10 px-0 md:px-10'>
             <h4 className='text-4xl font-semibold'>Here is a little background</h4>
